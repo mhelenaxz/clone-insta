1c24861e5abac42700ca57883794914248d9a456
Helena <mhelenabdss@gmail.com>
2023-10-10 08:54:31 -0300

    Meu commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="instagram-wrapper">
+      <div className="instagram-phone">
+        <img src="celular instagram.png" alt="celular" />
+      </div>
+
+      <div className="instagram-continue">
+        <div className="grupo">
+          <div className="logo">
+            <img src="nome instagram.png" alt="instagram" />
+          </div>
+          <div className="profile-foto">
+            <img src="foto perfil.webp" alt="perfil" />
+          </div>
+          <a href="#" className="instagram-login">Continue como mhelenaxz</a>
+          <a href="#" className="instagram-logout">Remover conta</a>
+        </div>
+        <div className="grupo">
+          <p className="not-account"> Não é mhelenaxz? </p>
+          <p className="notaccount">
+            <span className="link-blue">Alternar conta</span>
+            <span className="not-account"> ou </span>
+            <span className="link-blue">Inscreva-se</span>
+          </p>
+        </div>
+        <div className="baixe">
+          <p className="app">Baixe o aplicativo</p>
+          <div className="baixar">
+            <img src="apple-play.png" alt="aplicativos" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
